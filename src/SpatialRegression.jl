3694e43bd77a60c8dd2cbe2bc8dc0d03ec0a8af8
Alfonso Landeros <alandero@ucr.edu>
2025-11-05 15:29:28 -0800

    Implement coordinate conversion

--- src/SpatialRegression.jl
+++ src/SpatialRegression.jl
@@ -1,5 +1,56 @@
 module SpatialRegression
 
-# Write your package code here.
+using LinearAlgebra
+using StaticArrays
+
+#
+# COORDINATE CONVERSIONS
+#
+"""
+Convert Cartesian coordinates `s` to barycentric coordinates with respect
+to triangle vertices in `V`. Vertices are assumed to stored in columns.
+"""
+barycentric(s::AbstractVector, V) = barycentric!(zeros(eltype(s), 3), s, V)
+
+function barycentric!(out, s::AbstractVector{T}, V::AbstractMatrix{T}) where T <: AbstractFloat
+  @assert length(s) == 2
+  A = SMatrix{3,3,T,9}( # remember: column major
+    V[1,1], V[2,1], 1, # column 1
+    V[1,2], V[2,2], 1, # column 2
+    V[1,3], V[2,3], 1  # column 3
+  )
+  b = SVector{3,T}(s[1], s[2], 1)
+  out .= A \ b
+  return out
+end
+
+"""
+Converts Cartesian coordinates in `S` (stored in columns) to barycentric coordinates, using triangle vertices in `V`.
+"""
+function barycentric(S::AbstractMatrix, V)
+  out = zeros(eltype(S), 3, size(S, 2))
+  for (o, s) in zip(eachcol(out), eachcol(S))
+    barycentric!(o, s, V)
+  end
+  return out
+end
+
+"""
+Convert barycentric coordinates `a` with respect to triangle vertices `V` to Cartesian coordinates.
+Assumes vertices are stored in columns.
+"""
+cartesian(a::AbstractVector, V) = cartesian!(zeros(eltype(a), 2), a, V)
+cartesian!(out, a::AbstractVector{T}, V::AbstractMatrix{T}) where T <: AbstractFloat = mul!(out, V, a)
+
+"""
+Converts barycentric coordinates in `A` (stored in columns) to Cartesian coordinates, using triangle vertices in `V.`
+"""
+function cartesian(A::AbstractMatrix, V)
+  out = zeros(eltype(A), 2, size(A, 2))
+  for (o, a) in zip(eachcol(out), eachcol(A))
+    cartesian!(o, a, V)
+  end
+  return out
+end
 
 end
