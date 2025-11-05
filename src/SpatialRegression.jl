7af4639dac43d2e8a8048f14653b462baeaf3af6
Alfonso Landeros <alandero@ucr.edu>
2025-11-05 15:12:28 -0800

    Files generated by PkgTemplates

---
+++ src/SpatialRegression.jl
@@ -0,0 +1,5 @@
+module SpatialRegression
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SpatialRegression
+using Test
+
+@testset "SpatialRegression.jl" begin
+    # Write your tests here.
+end
