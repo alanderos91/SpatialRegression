3161be895ab8012a5f52968fa4377291d7f747e2
Alfonso Landeros <alandero@ucr.edu>
2025-11-22 01:44:09 -0800

    SYNC

--- examples/equilateral.jl
+++ examples/equilateral.jl
@@ -1,57 +1,64 @@
 import Pkg
-Pkg.activate(".")
+Pkg.activate("examples")
 
 using LinearAlgebra, Random, Statistics, Distributions
 using DelaunayTriangulation
 using SpatialRegression
 using CairoMakie
+using DataFrames
+using SpatialRegression.GLM
 
 # Vertices of equilateral triangle centered at (0, 0), inscribed in unit circle
 # Create boundary points of triangle area; use CCW orientation
 boundary_points = [[0.0, 1.0], [-sqrt(3)/2, -1/2], [sqrt(3)/2, -1/2]]
+init_area = 0.5 *
+  norm(boundary_points[1] - 0.5*(boundary_points[2] + boundary_points[3])) *
+  norm(boundary_points[3] - boundary_points[2])
 
-function symmetric_refinement(Δ)
+function equilateral_refinement(Δ, max_area)
   stats = statistics(Δ)
   N = stats.num_solid_vertices
   Δnew = refine!(deepcopy(Δ),
     # min_area = stats.smallest_area / 4 - sqrt(eps()),
-    max_area = stats.largest_area / 3 + sqrt(eps()),
-    # min_angle = 30.0,
-    # max_angle = 30.0,
+    max_area = max_area,
   )
   return Δnew
 end
 
-Δ₀ = triangulate_convex(boundary_points, [1, 2, 3]; delete_ghosts = true)
-triplot(Δ₀)
+function btest(n, p, Δ)
+  A = rand(Dirichlet(ones(3)), n)
+  V = [0 1; -sqrt(3)/2 -1/2; sqrt(3)/2 -1/2] |> Transpose |> Matrix{Float64}
+  S = SpatialRegression.cartesian(A, V)
+  Bfun(x, y, k, n) = sin(4*(x+k/n*pi/8)) + cos(6*y)
+  points = [[get_point(Δ, v)...] for v in each_solid_vertex(Δ)]
+  B = [log10(j+1)*Bfun(s[1], s[2], j, p) for j in 1:p, s in points]
+end
 
-Δ₁ = symmetric_refinement(Δ₀)
-triplot(Δ₁)
-
-Δ₂ = symmetric_refinement(Δ₁)
-triplot(Δ₂)
-
-function simulate_data(n, Δ)
+function simulate_data(n, p, Δ)
   # Sample from the standard 2-simplex, then convert to Cartesian coords
   A = rand(Dirichlet(ones(3)), n)
   V = [0 1; -sqrt(3)/2 -1/2; sqrt(3)/2 -1/2] |> Transpose |> Matrix{Float64}
   S = SpatialRegression.cartesian(A, V)
 
   # Simulate responses at each location according to true model
-  X = randn(n, 2); X[:, 1] .= 1
-  B = hcat([ [1.0, j*randn()] for j in 1:statistics(Δ).num_solid_vertices]...)
+  Bfun(x, y, k, n) = sin(4*(x+k/n*pi/8)) + cos(6*y)
+  points = [[get_point(Δ, v)...] for v in each_solid_vertex(Δ)]
+  id2vertex = Dict{Int,Int}(id => j for (j, id) in enumerate(each_solid_vertex(Δ)))
+  X = 1/p * randn(n, p); X[:, 1] .= 1
+  B = [sqrt(j)*Bfun(s[1], s[2], j, p) for j in 1:p, s in points]
   y = zeros(n)
   for i in 1:n
     s = @views S[:, i]
     j, k, l = find_triangle(Δ, s) |> sort
-    v1, v2, v3 = Δ.points[[j,k,l]]
+    j, k, l = id2vertex[j], id2vertex[k], id2vertex[l]
+    v1, v2, v3 = points[[j,k,l]]
     b1, b2, b3 = @views begin B[:, j], B[:, k], B[:, l] end
     a1, a2, a3 = SpatialRegression.barycentric(s, [v1 v2 v3])
     mixture = @views MixtureModel(
       [
-        Normal(dot(X[i, :], b1), 1/sqrt(n)),
-        Normal(dot(X[i, :], b2), 1/sqrt(n)),
-        Normal(dot(X[i, :], b3), 1/sqrt(n)),
+        Normal(dot(X[i, :], b1), 0.5),
+        Normal(dot(X[i, :], b2), 0.5),
+        Normal(dot(X[i, :], b3), 0.5),
       ],
       [a1, a2, a3]
     )
@@ -59,3 +66,73 @@ function simulate_data(n, Δ)
   end
   return y, X, S, B
 end
+
+#
+# TRIANGULATIONS
+#
+function get_tri_title(Δ)
+  stats = statistics(Δ)
+  return "$(stats.num_solid_triangles) triangles, $(stats.num_solid_vertices) vertices"
+end
+
+Δ₀ = triangulate_convex(boundary_points, [1, 2, 3]; delete_ghosts = false)
+Δ₁ = equilateral_refinement(Δ₀, 0.01*init_area)
+Δ₂ = equilateral_refinement(Δ₁, 0.001*init_area)
+Δs = (Δ₀, Δ₁, Δ₂)
+
+figtri = Figure(size = (400*length(Δs), 400))
+for (j, Δ) in enumerate(Δs)
+  ax = Axis(figtri[1,j], title = get_tri_title(Δ))
+  triplot!(ax, Δ)
+end
+figtri
+
+#
+# SCENARIO 1: Uniform over domain
+#
+n, p = 10^4, 10
+
+results1 = DataFrame(
+  vertices = Int[],
+  triangles = Int[],
+  niter = Int[],
+  objective = Float64[],
+  time = Float64[],
+  rmse_beta = Float64[],
+  rmse_resp = Float64[],
+)
+
+fig1 = Figure(size = (400*length(Δs), 400))
+for (j, Δ) in enumerate(Δs)
+  Random.seed!(1903)
+  stats = statistics(Δ)
+  local y, X, S, B0 = simulate_data(n, p, Δ)
+
+  ax = Axis(fig1[1,2*(j-1)+1], title = get_tri_title(Δ))
+  plotdata = scatter!(ax, S[1,:], S[2,:], color = y, markersize = 4.0)
+  Colorbar(fig1[1,2*j], plotdata, label = "Response, Y")
+
+  timed_result = @timed SpatialRegression.fitmodel(y, X, S, Δ;
+    family = Normal(),
+    link = IdentityLink(),
+    rho = 1.0,
+    tol = 1e-6,
+    backtrack = 100,
+    maxiter = 10^4
+  )
+  println()
+  niter, tobs, vmod = timed_result.value
+  logl = SpatialRegression.eval_loglikelihood(tobs, vmod)
+  timing = timed_result.time  
+  B = hcat([v.beta for v in vmod]...)
+  yhat = SpatialRegression.predict(X, S, vmod, Δ)
+  rmse_beta = sqrt(mean(abs2, B - B0))
+  rmse_resp = sqrt(mean(abs2, y - yhat))
+  push!(results1,
+    (length(vmod), stats.num_solid_triangles, niter, logl, timing, rmse_beta, rmse_resp)
+  )
+end
+fig1
+display(results1)
+
+
--- src/SpatialRegression.jl
+++ src/SpatialRegression.jl
@@ -65,6 +65,7 @@ function cartesian(A::AbstractMatrix, V)
   return out
 end
 
+exp_clamp(logf) = clamp()
 struct TriangleObs
   triangle::Tuple{Int,Int,Int} # indices in ascending order
   idx::Vector{Int}    # set of indices into original data (y, X)
@@ -78,19 +79,23 @@ end
 has_vertex(T::TriangleObs, j::Int) = j in T.triangle
 
 function create_triobs_sets(y, X, S, tri)
-  stats = statistics(tri)
-  vertex = tri.points
+  # Get points + mapping to solid vertices.
+  # The mapping is needed to ensure vertex label = index into some array
+  vertex = get_points(tri)
+  id2vertex = Dict{Int,Int}()
 
   # Matrix of vertices, stored along columns
-  V = Matrix{Float64}(undef, 2, stats.num_solid_vertices)
-  for j in each_solid_vertex(tri)
-    V[:, j] .= vertex[j]
+  V = Matrix{Float64}(undef, 2, length(vertex))
+  for (j, id) in enumerate(each_solid_vertex(tri))
+    V[:, j] .= vertex[id]
+    id2vertex[id] = j
   end
 
   # Retrieve each triangle, sorting indices in ascending order
   dict = Dict{Tuple{Int,Int,Int},Vector{Int}}()
   for (i, s) in enumerate(eachcol(S))
-    tri_ind = sort(find_triangle(tri, s))
+    j, k, l = find_triangle(tri, s)
+    tri_ind = sort((id2vertex[j], id2vertex[k], id2vertex[l]))
     if !haskey(dict, tri_ind)
       dict[tri_ind] = Int[]
     end
@@ -120,6 +125,7 @@ struct VertexModel{D <: Distribution, L <: Link}
   mu::Vector{Float64}       # mean parameter vector, μ = g⁻¹(η)
   workres::Vector{Float64}  # working residuals, (y - μ) g'(μ)
   neighbors::Vector{Int}    # index set representing neighboring vertices in penalty
+  triangles::Vector{Int}    # index set representing incident triangles
   weights::Vector{Float64}  # penalty weights, w in ∑ w P(B)
   rho::Float64              # penalty coefficient
   beta_new::Vector{Float64} # proposed update
@@ -127,18 +133,28 @@ end
 
 function create_vertexmodel_set(family::D, link::L, tri::Triangulation, tobs::Vector{TriangleObs}, nvars; rho::Real = 1.0) where {D,L}
   mv = VertexModel{D,L}[]
-  for j in each_solid_vertex(tri)
+  
+  # recreate mapping from triangulation labels to our labels
+  id2vertex = Dict{Int,Int}()
+  for (j, id) in enumerate(each_solid_vertex(tri))
+    id2vertex[id] = j
+  end
+  
+  # This assumes triangles in tobs are labeled using OUR scheme, not the one in the triangulation.
+  wmax = 0.0
+  for (j, id) in enumerate(each_solid_vertex(tri))
     # count the total number of samples incident with vertex j
-    nsamples = 0
+    nsamples, triangles = 0, Int[]
     for k in eachindex(tobs)
       if has_vertex(tobs[k], j)
         nsamples += length(tobs[k].y)
+        push!(triangles, k)
       end
     end
 
     # determine the number of incident vertices
-    neighbors = DelaunayTriangulation.iterated_neighbourhood(tri, j, 1) |>
-      collect |> sort!
+    neighbors = [id2vertex[u] for u in DelaunayTriangulation.iterated_neighbourhood(tri, id, 1)]
+    sort!(neighbors)
     nneighbors = length(neighbors)
 
     # allocate!
@@ -150,13 +166,24 @@ function create_vertexmodel_set(family::D, link::L, tri::Triangulation, tobs::Ve
     workres = zeros(nsamples)
     weights = ones(nneighbors)
 
+    for (k, other) in enumerate(DelaunayTriangulation.iterated_neighbourhood(tri, id, 1))
+      weights[k] = 1 / DelaunayTriangulation.dist(get_point(tri, id), get_point(tri, other))
+      wmax = max(wmax, weights[k])
+    end
+
     push!(mv, VertexModel(family, link, j,
         d, beta, gamma, eta, mu, workres,
-        neighbors, weights, rho, similar(beta)
+        neighbors, triangles, weights, rho, similar(beta)
       )
     )
   end
-  return sort!(mv, by = v -> v.index)
+  
+  # sort!(mv, by = v -> v.index)
+  for v in mv
+    v.weights .= v.weights / wmax
+  end
+
+  return mv
 end
 
 function loglik_obs(v::VertexModel, y, x)
@@ -164,6 +191,20 @@ function loglik_obs(v::VertexModel, y, x)
   μ, _ = GLM.inverselink(v.link, η)
   logl = GLM.loglik_obs(v.family, y, μ, 1, 1)
   return logl
+end
+
+function stable_logsumexp(a, f)
+  s, c = stable_explogsum(a, f)
+  return log(a[s]) + f[s] + log(c)
+end
+
+function stable_explogsum(a, f)
+  s = argmin(f)
+  c = zero(eltype(f))
+  for k in eachindex(f)
+    c += a[k] / a[s] * exp(f[k] - f[s])
+  end
+  return s, c
 end
 
 function eval_loglikelihood(tobs, vmod)
@@ -174,60 +215,37 @@ function eval_loglikelihood(tobs, vmod)
     A, y, X = T.A, T.y, T.X
     v1, v2, v3 = vmod[j], vmod[k], vmod[l]
     @views for i in eachindex(y)
-      f1 = loglik_obs(v1, y[i], X[i, :]) |> exp
-      f2 = loglik_obs(v2, y[i], X[i, :]) |> exp
-      f3 = loglik_obs(v3, y[i], X[i, :]) |> exp
-      f = A[1,i]*f1 + A[2,i]*f2 + A[3,i]*f3
-      logl += ifelse(iszero(f), zero(f), log(f))
+      # evaluate log-likelihood at each vertex
+      a1, f1 = A[1,i], loglik_obs(v1, y[i], X[i, :])
+      a2, f2 = A[2,i], loglik_obs(v2, y[i], X[i, :])
+      a3, f3 = A[3,i], loglik_obs(v3, y[i], X[i, :])
+      
+      # shift log(∑ⱼ αⱼ exp(fⱼ)) by the most negative log-likelihood
+      f = (f1, f2, f3)
+      a = (a1, a2, a3)
+      logl += stable_logsumexp(a, f)
     end
   end
   # Penalty
   for v in vmod
     for (idx, k) in enumerate(v.neighbors)
       u = vmod[k]
-      logl -= v.rho/2 * v.weights[idx] * sum(abs2, v.beta - u.beta)
+      logl -= v.rho/4 * v.weights[idx] * sum(abs2, v.beta - u.beta)
     end
   end
   return logl
 end
 
-function eval_surrogate(v::VertexModel, vmod, tobs)
-  logl = zero(Float64)
-  # Log-Likelihood
-  for T in tobs
-    if !has_vertex(T, v.index) continue end
-    j, k, l = T.triangle; pos = findfirst(==(v.index), T.triangle)
-    v1, v2, v3 = vmod[j], vmod[k], vmod[l]
-    A, y, X = T.A, T.y, T.X
-    @views for i in eachindex(y)
-      f1 = loglik_obs(v1, y[i], X[i, :])
-      f2 = loglik_obs(v2, y[i], X[i, :])
-      f3 = loglik_obs(v3, y[i], X[i, :])
-      z1 = A[1,i] * ifelse(f1 > -600, exp(f1), one(f1))
-      z2 = A[2,i] * ifelse(f2 > -600, exp(f2), one(f2))
-      z3 = A[3,i] * ifelse(f3 > -600, exp(f3), one(f3))
-      zsum = z1 + z2 + z3
-      z, f = ifelse(v.index == j, (z1, f1), ifelse(v.index == k, (z2, f2), (z3, f3)))
-      logl += z / zsum * f + z / zsum * (log(A[pos,i]) + log(zsum) - log(z))
-      if isnan(logl)
-        @show i, z1, z2, z3, zsum, f
-        error()
-      end
-    end
-  end
-  # Penalty
-  penalty = zero(logl)
-  for (idx, γ) in enumerate(eachcol(v.gamma))
-    penalty -= v.rho * v.weights[idx] * sum(abs2, v.beta - γ)
-  end
-  return logl + penalty
+function eval_surrogate(tobs, vmod, j)
+  v = vmod[j]
+  eval_surrogate(v.beta, v.index, v.gamma, v.weights, v.rho, vmod, tobs)
 end
 
-function G(beta, index, gamma, weights, rho, vmod, tobs)
-  logl = zero(Float64)
+function eval_surrogate(beta, index, gamma, weights, rho, vmod, tobs)
+  logl, v = zero(Float64), vmod[index]
   # Log-Likelihood
-  for T in tobs
-    if !has_vertex(T, index) continue end
+  for triidx in v.triangles
+    T = tobs[triidx]
     # j, k, l = T.triangle
     pos1 = findfirst(==(index), T.triangle)
     if pos1 == 1
@@ -247,24 +265,24 @@ function G(beta, index, gamma, weights, rho, vmod, tobs)
     @views for i in eachindex(T.y)
       x = X[i,:]
       η1, η2, η3 = dot(x, beta1), dot(x, beta2), dot(x, beta3)
+      
       μ1, _, _ = GLM.inverselink(link, η1)  
       μ2, _, _ = GLM.inverselink(link, η2)
       μ3, _, _ = GLM.inverselink(link, η3)
-      f1 = GLM.loglik_obs(family, y[i], μ1, 1, 1)
-      f2 = GLM.loglik_obs(family, y[i], μ2, 1, 1)
-      f3 = GLM.loglik_obs(family, y[i], μ3, 1, 1)
-
-      z1 = A[pos1,i] * ifelse(f1 > -600, exp(f1), one(f1))
-      z2 = A[pos2,i] * ifelse(f2 > -600, exp(f2), one(f2))
-      z3 = A[pos3,i] * ifelse(f3 > -600, exp(f3), one(f3))
-
-      zsum = z1 + z2 + z3
+      
+      a1, f1 = A[pos1, i], GLM.loglik_obs(family, y[i], μ1, 1, 1)
+      a2, f2 = A[pos2, i], GLM.loglik_obs(family, y[i], μ2, 1, 1)
+      a3, f3 = A[pos3, i], GLM.loglik_obs(family, y[i], μ3, 1, 1)
+      as = (a1, a2, a3)
+      fs = (f1, f2, f3)
+      s, c = stable_explogsum(as, fs)
+      zweight = as[1] / as[s] * exp(fs[1] - fs[s]) / c
 
       η = dot(x, beta)
       μ, _, _ = GLM.inverselink(link, η)
       f = GLM.loglik_obs(family, y[i], μ, 1, 1)
 
-      logl += z1 / zsum * f + z1 / zsum * (log(A[pos1,i]) + log(zsum) - log(z1))
+      logl += zweight * f + zweight * (log(a1) + log(inv(zweight)))
     end
   end
   # Penalty
@@ -275,6 +293,46 @@ function G(beta, index, gamma, weights, rho, vmod, tobs)
   return logl + penalty
 end
 
+function eval_gamma!(vmod)
+  for v in vmod
+    β = v.beta
+    Γ = v.gamma
+    for (idx, k) in enumerate(v.neighbors)
+      if v.index == k continue end
+      βₖ = vmod[k].beta
+      @views begin
+        @. Γ[:, idx] = 1//2 * (β + βₖ)
+      end
+    end
+  end
+end
+
+function initialize_coefficients!(tobs, vmod)
+  for v in vmod
+    initialize_coefficients!(v, tobs, vmod)
+  end
+end
+
+function initialize_coefficients!(v::VertexModel, tobs, vmod)
+  p = length(v.beta)
+  for triidx in v.triangles
+    T = tobs[triidx]
+    j, k, l = T.triangle
+    
+    # Solve linear system associated with triangle
+    η = GLM.linkfun.(v.link, T.y)
+    X = [Diagonal(T.A[1,:])*T.X Diagonal(T.A[2,:])*T.X Diagonal(T.A[3,:])*T.X]
+    β = [vmod[j].beta; vmod[k].beta; vmod[l].beta]
+    β .= X \ η
+
+    # Average solutions over incident triangles for each vertex
+    for (r, u) in enumerate((vmod[j], vmod[k], vmod[l]))
+      u.beta .+= 1/length(u.triangles) * β[p*(r-1)+1 : p*r]
+    end
+  end
+  return nothing
+end
+
 function fitmodel(yfull, Xfull, Sfull, tri;
     maxiter::Int = 100,
     backtrack::Int = 5,
@@ -282,12 +340,13 @@ function fitmodel(yfull, Xfull, Sfull, tri;
     family = Normal(),
     link = GLM.canonicallink(family),
     rho = 1.0,
-    intercept = all(isequal(1), view(Xfull, :, 1)),
+    # intercept = all(isequal(1), view(Xfull, :, 1)),
   )
   # Initialize
   nvars = size(Xfull, 2)
   tobs = create_triobs_sets(yfull, Xfull, Sfull, tri)
   vmod = create_vertexmodel_set(family, link, tri, tobs, nvars; rho = rho)
+  # initialize_coefficients!(tobs, vmod)
   logl = eval_loglikelihood(tobs, vmod)
   logl_prev = zero(logl)
   ∇L = zeros(nvars)
@@ -298,33 +357,27 @@ function fitmodel(yfull, Xfull, Sfull, tri;
   while iter < maxiter && abs(logl - logl_prev) > (1 + abs(logl_prev)) * tol
     iter += 1
 
+    # Update local averaged estimates, γₙⱼₖ
+    eval_gamma!(vmod)
+
     # Visit each vertex once to update local parameters
     for v in vmod
       # Setup local variables to match notation
       β = v.beta
       Γ = v.gamma
       d = v.d
-      D = Diagonal(d)
       r = v.workres
       η = v.eta
       μ = v.mu
       w = v.weights
 
-      # Update local averaged estimates, γₙⱼₖ
-      for (idx, k) in enumerate(v.neighbors)
-        if v.index == k continue end
-        βₖ = vmod[k].beta
-        @views begin
-          @. Γ[:, idx] = 1//2 * (β + βₖ)
-        end
-      end
-      objective = eval_surrogate(v, vmod, tobs)
+      objective = eval_surrogate(v.beta, v.index, Γ, w, v.rho, vmod, tobs)
       isinf(objective) && display(v.beta)
       
       fill!(∇L, 0); fill!(∇²L, 0)
       idx = 1
-      for T in tobs
-        if !has_vertex(T, v.index) continue end
+      for triidx in v.triangles
+        T = tobs[triidx]
         j, k, l = T.triangle
         A, y, X = T.A, T.y, T.X
         v1, v2, v3 = vmod[j], vmod[k], vmod[l]
@@ -332,21 +385,20 @@ function fitmodel(yfull, Xfull, Sfull, tri;
         # Compute weights and working residuals
         for i in eachindex(y)
           # exp(f) may be close to 0; replace with A[]
-          f1 = @views loglik_obs(v1, y[i], X[i, :])
-          f2 = @views loglik_obs(v2, y[i], X[i, :])
-          f3 = @views loglik_obs(v3, y[i], X[i, :])
+          a1, f1 = A[1, i], @views loglik_obs(v1, y[i], X[i, :])
+          a2, f2 = A[2, i], @views loglik_obs(v2, y[i], X[i, :])
+          a3, f3 = A[3, i], @views loglik_obs(v3, y[i], X[i, :])
+          as = (a1, a2, a3)
+          fs = (f1, f2, f3)
 
-          z1 = A[1,i] * ifelse(f1 > -745, exp(f1), one(f1))
-          z2 = A[2,i] * ifelse(f2 > -745, exp(f2), one(f2))
-          z3 = A[3,i] * ifelse(f3 > -745, exp(f3), one(f3))
-
-          z = ifelse(v.index == j, z1, ifelse(v.index == k, z2, z3))
-          zsum = z1 + z2 + z3
+          t = findfirst(==(v.index), T.triangle)
+          s, c = stable_explogsum(as, fs)
+          zweight = as[t] / as[s] * exp(fs[t] - fs[s]) / c
 
           η[idx] = @views dot(X[i, :], β)
           μ[idx], dμdη = GLM.inverselink(v.link, η[idx])
           r[idx] = (y[i] - μ[idx]) / dμdη
-          d[idx] = z/zsum * dμdη^2 / GLM.glmvar(v.family, μ[idx])
+          d[idx] = zweight * dμdη^2 / GLM.glmvar(v.family, μ[idx])
           idx += 1
         end
 
@@ -357,9 +409,11 @@ function fitmodel(yfull, Xfull, Sfull, tri;
         ∇L .+= X' * Dj * rj
         ∇²L .+= X' * Dj * X
       end
-      idxpen = (1+intercept):nvars
-      ∇L[idxpen] .= ∇L[idxpen] - 2*v.rho*(sum(w)*β[idxpen] - Γ[idxpen,:]*w)
-      ∇²L[idxpen,idxpen] .= ∇²L[idxpen,idxpen] + 2*v.rho*sum(w)*I
+      # idxpen = (1+intercept):nvars
+      # ∇L[idxpen] .= ∇L[idxpen] - 2*v.rho*(sum(w)*β[idxpen] - Γ[idxpen,:]*w)
+      # ∇²L[idxpen,idxpen] .= ∇²L[idxpen,idxpen] + 2*v.rho*sum(w)*I
+      ∇L .= ∇L - 2*v.rho*(sum(w)*β - Γ*w)
+      ∇²L .= ∇²L + 2*v.rho*sum(w)*I
       # ∇G = ForwardDiff.gradient(beta -> G(beta, v.index, Γ, w, v.rho, vmod, tobs), β)
       # ∇²G = -ForwardDiff.hessian(beta -> G(beta, v.index, Γ, w, v.rho, vmod, tobs), β)
 
@@ -368,6 +422,7 @@ function fitmodel(yfull, Xfull, Sfull, tri;
       # display(∇²L)
       # Compute the search direction
       search_direction .= Symmetric(∇²L) \ ∇L
+      # @show norm(∇L), norm(search_direction)
 
       # Backtracking line search
       t = 1.0
@@ -377,7 +432,7 @@ function fitmodel(yfull, Xfull, Sfull, tri;
         @. v.beta_new = β + t * search_direction
         # @assert β === v.beta
         # objective_new = eval_surrogate(v, vmod, tobs)
-        objective_new = G(v.beta_new, v.index, Γ, w, v.rho, vmod, tobs)
+        objective_new = eval_surrogate(v.beta_new, v.index, Γ, w, v.rho, vmod, tobs)
         # @show objective_new, objective
         if objective_new >= objective
           break
@@ -386,6 +441,8 @@ function fitmodel(yfull, Xfull, Sfull, tri;
         else
           @show t, objective_new, objective
           error("Backtracking failed at iteration $(iter) for vertex $(v.index) after $(step) attempts!")
+          v.beta_new .= β
+          break
         end
       end
       # β .= v.beta_new
@@ -401,11 +458,31 @@ function fitmodel(yfull, Xfull, Sfull, tri;
     # Evaluate log-likelihood
     logl_prev = logl
     logl = eval_loglikelihood(tobs, vmod)
-    rel = abs(logl - logl_prev) / (1 + abs(logl_prev))
-    @show iter, logl, logl_prev, rel
+    # rel = abs(logl - logl_prev) / (1 + abs(logl_prev))
+    # @show iter, logl, logl_prev, rel
   end
-
+  @show norm(∇L)
   return iter, tobs, vmod
+end
+
+function predict(X, S, vmod, tri)
+  id2vertex = Dict{Int,Int}(id => j for (j, id) in enumerate(each_solid_vertex(tri)))
+  points = get_points(tri)
+  yhat = zeros(size(X, 1))
+  @views for i in axes(X, 1)
+    s = S[:, i]
+    x = X[i, :]
+    j, k, l = find_triangle(tri, s) |> sort
+    p, q, r = points[j], points[k], points[l]
+    j, k, l = id2vertex[j], id2vertex[k], id2vertex[l]
+    v, u, w = vmod[j], vmod[k], vmod[l]
+    a, b, c = barycentric(s, [p q r])
+    yhat[i] = 
+      GLM.linkinv(v.link, a*dot(x, v.beta)) +
+      GLM.linkinv(u.link, b*dot(x, u.beta)) +
+      GLM.linkinv(w.link, c*dot(x, w.beta))
+  end
+  return yhat
 end
 
 export TriangleObs, create_triobs_sets,
