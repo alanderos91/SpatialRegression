5b0699d361d0b2e7a322e56cf69010671efa7457
Alfonso Landeros <alandero@ucr.edu>
2026-01-31 23:44:11 -0800

    Update examples

--- examples/equilateral.jl
+++ examples/equilateral.jl
@@ -1,11 +1,11 @@
 import Pkg
-Pkg.activate("examples")
+Pkg.activate(".")
 
 using LinearAlgebra, Random, Statistics, Distributions
 using DelaunayTriangulation
 using SpatialRegression
 using CairoMakie
-using DataFrames
+using DataFrames, PrettyTables
 using SpatialRegression.GLM
 
 # Vertices of equilateral triangle centered at (0, 0), inscribed in unit circle
@@ -16,10 +16,7 @@ init_area = 0.5 *
   norm(boundary_points[3] - boundary_points[2])
 
 function equilateral_refinement(Δ, max_area)
-  stats = statistics(Δ)
-  N = stats.num_solid_vertices
   Δnew = refine!(deepcopy(Δ),
-    # min_area = stats.smallest_area / 4 - sqrt(eps()),
     max_area = max_area,
   )
   return Δnew
@@ -34,7 +31,21 @@ function btest(n, p, Δ)
   B = [log10(j+1)*Bfun(s[1], s[2], j, p) for j in 1:p, s in points]
 end
 
-function simulate_data(n, p, Δ)
+function create_component(family::Normal, link, η)
+  σ = scale(family)
+  μ = GLM.linkinv(link, η)
+  return Normal(μ, σ)
+end
+
+function create_component(::Binomial, link, η)
+  return Bernoulli(GLM.linkinv(link, η))
+end
+
+function create_component(::Poisson, link, η)
+  return Poisson(GLM.linkinv(link, η))
+end
+
+function simulate_data(n, p, Δ, family, link)
   # Sample from the standard 2-simplex, then convert to Cartesian coords
   A = rand(Dirichlet(ones(3)), n)
   V = [0 1; -sqrt(3)/2 -1/2; sqrt(3)/2 -1/2] |> Transpose |> Matrix{Float64}
@@ -44,21 +55,23 @@ function simulate_data(n, p, Δ)
   Bfun(x, y, k, n) = sin(4*(x+k/n*pi/8)) + cos(6*y)
   points = [[get_point(Δ, v)...] for v in each_solid_vertex(Δ)]
   id2vertex = Dict{Int,Int}(id => j for (j, id) in enumerate(each_solid_vertex(Δ)))
-  X = 1/p * randn(n, p); X[:, 1] .= 1
-  B = [sqrt(j)*Bfun(s[1], s[2], j, p) for j in 1:p, s in points]
+  X = 1/p*rand(n, p)
+  X[:, 1] .= 1
+  B = [Bfun(s[1], s[2], j, p) for j in 1:p, s in points]
+  B[1, :] .= 4.0
   y = zeros(n)
   for i in 1:n
     s = @views S[:, i]
-    j, k, l = find_triangle(Δ, s) |> sort
+    j, k, l = find_triangle(Δ, s; concavity_protection = true) |> sort
     j, k, l = id2vertex[j], id2vertex[k], id2vertex[l]
     v1, v2, v3 = points[[j,k,l]]
     b1, b2, b3 = @views begin B[:, j], B[:, k], B[:, l] end
     a1, a2, a3 = SpatialRegression.barycentric(s, [v1 v2 v3])
     mixture = @views MixtureModel(
       [
-        Normal(dot(X[i, :], b1), 0.5),
-        Normal(dot(X[i, :], b2), 0.5),
-        Normal(dot(X[i, :], b3), 0.5),
+        create_component(family, link, dot(X[i, :], b1)),
+        create_component(family, link, dot(X[i, :], b2)),
+        create_component(family, link, dot(X[i, :], b3)),
       ],
       [a1, a2, a3]
     )
@@ -75,52 +88,73 @@ function get_tri_title(Δ)
   return "$(stats.num_solid_triangles) triangles, $(stats.num_solid_vertices) vertices"
 end
 
-Δ₀ = triangulate_convex(boundary_points, [1, 2, 3]; delete_ghosts = false)
-Δ₁ = equilateral_refinement(Δ₀, 0.01*init_area)
-Δ₂ = equilateral_refinement(Δ₁, 0.0005*init_area)
-Δs = (Δ₀, Δ₁, Δ₂)
+#
+# PLOTS
+#
+function plot_compare_fitted(instances; markersize = 4.0, kwargs...)
+  local W = 400; H = 250
+  local NROW = 2; NSCENARIO = length(instances)
 
-figtri = Figure(size = (400*length(Δs), 400))
-for (j, Δ) in enumerate(Δs)
-  ax = Axis(figtri[1,j], title = get_tri_title(Δ))
-  triplot!(ax, Δ)
+  # Initialize figure with column labels
+  fig = Figure(size = (W * NSCENARIO, H * NROW))
+  Label(fig[2,1], "Observed", font = :bold, rotation = pi/2, tellheight = false)
+  Label(fig[3,1], "Predicted", font = :bold, rotation = pi/2, tellheight = false)
+  cr = Observable((0.0, 1.0))
+  for (j, prob) in enumerate(instances)
+    # Retrieve information about the instance
+    y, yhat, Δ, S = prob.response, prob.prediction, prob.triangulation, prob.location
+
+    # Update minimum and maximum values for color scale
+    cr[] = (min(minimum(y), minimum(yhat)), max(maximum(y), maximum(yhat)))
+
+    # Add a row label with triangulation characteristics
+    Label(fig[1,1+j], get_tri_title(Δ), font = :bold, tellwidth = false)
+
+    # Add a panel for observed data
+    ax = Axis(fig[2,1+j])
+    scatter!(ax, S[1,:], S[2,:]; color = y, colorrange = cr, markersize, kwargs...)
+
+    # Predicted
+    ax = Axis(fig[3,1+j])
+    scatter!(ax, S[1,:], S[2,:]; color = yhat, colorrange = cr, markersize, kwargs...)
+  end
+  Colorbar(fig[2:3, 2+NSCENARIO], colorrange = cr, vertical = true)
+  return fig
 end
-figtri
 
 #
-# SCENARIO 1: Uniform over domain
+# MISC
 #
-n, p = 10^3, 10
+function init_table()
+  return DataFrame(
+    family = String[],
+    vertices = Int[],
+    triangles = Int[],
+    niter = Int[],
+    objective = Float64[],
+    time = Float64[],
+    rmse_beta = Float64[],
+    rmse_resp = Float64[],
+  )
+end
 
-results1 = DataFrame(
-  vertices = Int[],
-  triangles = Int[],
-  niter = Int[],
-  objective = Float64[],
-  time = Float64[],
-  rmse_beta = Float64[],
-  rmse_resp = Float64[],
-)
+function run_benchmark!(generate_data, results, Δ, family, link, seed)
+  # Sample from data generating function
+  Random.seed!(seed)
+  local y, X, S, B0 = generate_data(Δ)
 
-fig1 = Figure(size = (400*length(Δs), 400))
-for (j, Δ) in enumerate(Δs)
-  Random.seed!(1903)
-  stats = statistics(Δ)
-  local y, X, S, B0 = simulate_data(n, p, Δ)
-
-  ax = Axis(fig1[1,2*(j-1)+1], title = get_tri_title(Δ))
-  plotdata = scatter!(ax, S[1,:], S[2,:], color = y, markersize = 4.0)
-  Colorbar(fig1[1,2*j], plotdata, label = "Response, Y")
-
+  # Fit a model with our MM algorithm
   timed_result = @timed SpatialRegression.fitmodel(y, X, S, Δ;
-    family = Normal(),
-    link = IdentityLink(),
+    family = family,
+    link = link,
     rho = 1.0,
     tol = 1e-6,
     backtrack = 100,
     maxiter = 10^4
   )
-  println()
+
+  # Collect results and write to DataFrame
+  stats = statistics(Δ)
   niter, tobs, vmod = timed_result.value
   logl = SpatialRegression.eval_loglikelihood(tobs, vmod)
   timing = timed_result.time  
@@ -128,11 +162,101 @@ for (j, Δ) in enumerate(Δs)
   yhat = SpatialRegression.predict(X, S, vmod, Δ)
   rmse_beta = sqrt(mean(abs2, B - B0))
   rmse_resp = sqrt(mean(abs2, y - yhat))
-  push!(results1,
-    (length(vmod), stats.num_solid_triangles, niter, logl, timing, rmse_beta, rmse_resp)
+  push!(results,
+    (
+      family |> typeof |> nameof |> string,
+      length(vmod), stats.num_solid_triangles, 
+      niter, logl, timing, rmse_beta, rmse_resp
+    )
   )
+
+  # Return information about this instance
+  info = (;
+    response = y,
+    features = X,
+    location = S,
+    triangulation = Δ,
+    prediction = yhat,
+    niter, logl, timing, rmse_beta, rmse_resp,
+  )
+  return info
 end
-fig1
-display(results1)
 
+function run_benchmarks(scenario, seed)
+  results = init_table()
+  instances = NamedTuple[]
+  for Δ in scenario.triangulations
+    prob = run_benchmark!(scenario.data, results, Δ, scenario.family, scenario.link, seed)
+    push!(instances, prob)
+  end
+  return results, instances
+end
 
+Random.seed!(1903)
+Δ₀ = triangulate_convex(boundary_points, [1, 2, 3]; delete_ghosts = false)
+Δ₁ = equilateral_refinement(Δ₀, 0.01*init_area)
+Δ₂ = equilateral_refinement(Δ₁, 0.001*init_area)
+Δs = (Δ₀, Δ₁, Δ₂);
+
+figtri = Figure(size = (400*length(Δs), 400));
+for (j, Δ) in enumerate(Δs)
+  ax = Axis(figtri[1,j], title = get_tri_title(Δ))
+  triplot!(ax, Δ)
+end
+figtri
+save("Figure-Meshes.pdf", figtri)
+
+# RUN BENCHMARKS
+n, p = 10^4, 10
+seed = 1903
+scenarios = [
+  #
+  # SCENARIO 1: Uniform over domain, Normal response
+  #
+  (;
+    name    = "Balanced_Normal",
+    data    = Δ -> simulate_data(n, p, Δ, Normal(0.0, 0.1), IdentityLink()),
+    family  = Normal(),
+    link    = IdentityLink(),
+    triangulations = Δs,
+  ),
+  #
+  # SCENARIO 2: Uniform over domain, Binomial response
+  #
+  (;
+    name    = "Balanced_Binomial",
+    data    = Δ -> simulate_data(n, p, Δ, Binomial(), LogitLink()),
+    family  = Binomial(),
+    link    = LogitLink(),
+    triangulations = Δs,
+  ),
+  #
+  # SCENARIO 3: Uniform over domain, Poisson response
+  #
+  (;
+    name    = "Balanced_Poisson",
+    data    = Δ -> simulate_data(n, p, Δ, Poisson(), LogLink()),
+    family  = Poisson(),
+    link    = LogLink(),
+    triangulations = Δs,
+  ),
+];
+
+fig = Figure[]
+tbl = DataFrame[]
+ins = []
+for scenario in scenarios
+  results, instances = run_benchmarks(scenario, seed)
+  figure = plot_compare_fitted(instances)
+  save("Figure-$(scenario.name).pdf", figure)
+  push!(tbl, results)
+  push!(fig, figure)
+  push!(ins, instances)
+end
+
+foreach(display, fig)
+foreach(display, tbl)
+
+open("Table-Balanced.txt", "w") do io
+  pretty_table(io, vcat(tbl...); backend = :latex)
+end
--- examples/hmda.jl
+++ examples/hmda.jl
@@ -1,6 +1,15 @@
-using DataFrames, CSV, Arrow, ZipFile, Shapefile
-using Tables
+using DataFrames, CSV, Arrow, ZipFile, PrettyTables
+using Tables, TableOperations
 using CategoricalArrays
+using LibGEOS, Shapefile
+import GeometryOps as GO
+import ArchGDAL as AD
+
+using LinearAlgebra, Random, Statistics, StatsBase, StatsModels, Distributions
+using DelaunayTriangulation
+using SpatialRegression
+using CairoMakie
+using SpatialRegression.GLM
 
 const INCLUDED_AGE_LEVELS = ["<25", "25-34", "35-44", "45-54", "55-64", "65-74", ">74"]
 const EXCLUDED_DTI_VALUES = ["Exempt", "NA"]
@@ -9,6 +18,45 @@ const INCLUDED_RACE_LEVELS = ["White", "Asian", "Black or African American"]
 const EXCLUDED_STATE_CODES = ["AK", "HI", "PR", "VI", "NA"]
 const INCLUDED_SEX_LEVELS = ["Male", "Female"]
 
+# See Table C.3 in Supplementary Materials of Yu et al 2025.
+# Data: https://s3.amazonaws.com/cfpb-hmda-public/prod/snapshot-data/2019/2019_public_lar_csv.zip
+# Source: https://ffiec.cfpb.gov/data-publication/snapshot-national-loan-level-dataset/2019
+# Data Dictionary: https://ffiec.cfpb.gov/documentation/publications/loan-level-datasets/lar-data-fields
+const COLUMN_SPEC = Dict(
+  "activity_year"   => Union{Missing, Int16},
+  "lei"             => Union{Missing, String31},
+  "derived_msa_md"  => Union{Missing, Int},
+  "state_code"      => Union{Missing, String3},
+  "county_code"     => Union{Missing, String7},
+  "census_tract"    => Union{Missing, String15},
+  "derived_dwelling_category"       => Union{Missing, String},
+  "business_or_commercial_purpose"  => Union{Missing, Int16},
+  "loan_term"                       => Union{Missing, String},
+  "loan_type"                       => Union{Missing, Int16},
+  "derived_loan_product_type"       => Union{Missing, String31},
+  "applicant_ethnicity_observed"    => Union{Missing, Int16},
+  "derived_ethnicity"               => Union{Missing, String31},
+  "applicant_race_observed" => Union{Missing, Int16},
+  "derived_race"            => Union{Missing, String},
+  "applicant_sex_observed"  => Union{Missing, Int16},
+  "derived_sex"             => Union{Missing, String31},
+  "applicant_age"           => Union{Missing, String7},     # Need to handle categories > age 64
+  "income"                  => Union{Missing, String15},    # Income, thousands of dollars
+  "debt_to_income_ratio"    => Union{Missing, String15},    # DTI
+  #
+  "tract_population"                  => Union{Missing, Int32},   # Tpop
+  "tract_minority_population_percent" => Union{Missing, Float64}, # Tminority
+  "tract_to_msa_income_percentage"    => Union{Missing, Int16},   # Tincome
+  "ffiec_msa_md_median_family_income" => Union{Missing, Int32},   # Mincome
+  "action_taken"                      => Union{Missing, Int8},    # 1-8; 3 indicates application was denied
+)
+const ARCHIVE_FILENAME = joinpath("data", "2019_public_lar_csv.zip")
+const CSV_FILENAME = "2019_public_lar_csv.csv"
+const ARROW_FILENAME = joinpath("data", "hmda2019.arrow")
+const SHAPEFILE = joinpath("data", "cb_2019_us_tract_500k.zip")
+const VSIZIP_SHAPEFILE = joinpath("/vsizip", "data", "cb_2019_us_tract_500k.zip", "cb_2019_us_tract_500k.shp")
+const ARROW_CLEANED = joinpath("data", "hmda2019_clean.arrow")
+
 function empty_factor(::Type{T}, levels::Vector{T}; ordered = false) where T
   return CategoricalVector{T}(undef, 0; levels = levels, ordered = ordered)
 end
@@ -16,78 +64,324 @@ end
 function included_in_study(row)
   global INCLUDED_AGE_LEVELS, EXCLUDED_DTI_VALUES, INCLUDED_ETHNICITY_LEVELS, INCLUDED_RACE_LEVELS, EXCLUDED_STATE_CODES, INCLUDED_SEX_LEVELS
   !any(ismissing, row) &&
-    row.derived_msa_md != "0" &&
-    row.census_tract != "NA" &&
-    row.loan_term != "NA" &&
-    row.business_or_commercial_purpose == "2" &&
+    row.derived_msa_md != 0 &&
+    row.business_or_commercial_purpose == 2 &&
     row.derived_sex ∈ INCLUDED_SEX_LEVELS &&
     row.applicant_age ∈ INCLUDED_AGE_LEVELS &&
-    row.debt_to_income_ratio ∉ EXCLUDED_DTI_VALUES &&
+    !isnothing(tryparse(Int, row.debt_to_income_ratio)) && # use numeric ONLY
     row.derived_ethnicity ∈ INCLUDED_ETHNICITY_LEVELS &&
     contains(row.derived_dwelling_category, "Single Family") &&
     row.derived_race ∈ INCLUDED_RACE_LEVELS &&
-    row.state_code ∉ EXCLUDED_STATE_CODES
+    row.state_code ∉ EXCLUDED_STATE_CODES &&
+    row.loan_term != "Exempt" &&
+    row.action_taken ∈ (1, 3) # 1 - Loan originated; 3 - Application denied
 end
 
 function hmda2019_csv2arrow()
-  # See Table C.3 in Supplementary Materials of Yu et al 2025.
-  # Data: https://s3.amazonaws.com/cfpb-hmda-public/prod/snapshot-data/2019/2019_public_lar_csv.zip
-  # Source: https://ffiec.cfpb.gov/data-publication/snapshot-national-loan-level-dataset/2019
-  # Data Dictionary: https://ffiec.cfpb.gov/documentation/publications/loan-level-datasets/lar-data-fields
-  COLUMN_SPEC = [
-    "activity_year", "lei", "derived_msa_md", "state_code", "county_code",
-    "census_tract", "derived_dwelling_category", "business_or_commercial_purpose",
-    "loan_term", "loan_type", "derived_loan_product_type",
-    "applicant_ethnicity_observed", "derived_ethnicity",
-    "applicant_race_observed", "derived_race",
-    "applicant_sex_observed", "derived_sex", 
-    "applicant_age",                      # Need to handle categories > age 64
-    "income",                             # Income, thousands of dollars
-    "debt_to_income_ratio",               # DTI
-    #
-    "tract_population",                   # Tpop
-    "tract_minority_population_percent",  # Tminority
-    "tract_to_msa_income_percentage",     # Tincome
-    "ffiec_msa_md_median_family_income",  # Mincome
-    "action_taken"                        # 1-8; 3 indicates application was denied
-  ]
-  ARCHIVE_FILENAME = joinpath("data", "2019_public_lar_csv.zip")
-  CSV_FILENAME = "2019_public_lar_csv.csv"
-  ARROW_FILENAME = joinpath("data", "hmda2019.arrow")
+  global COLUMN_SPEC, ARCHIVE_FILENAME, ARROW_FILENAME, CSV_FILENAME
   archive = ZipFile.Reader(ARCHIVE_FILENAME) # make sure to clean this up!
   file = filter(x -> x.name == CSV_FILENAME, archive.files) |> first
-  itr = Iterators.filter(
-    included_in_study,
-    CSV.Rows(file, header = true, select = COLUMN_SPEC)
+  itr = CSV.Rows(file,
+    header = true,
+    select = collect(keys(COLUMN_SPEC)),
+    types = COLUMN_SPEC,
+    reusebuffer = false,
+    missingstring = ["", "NA", "Na", "na"],
   )
-  Arrow.write(ARROW_FILENAME, itr, compress = :zstd)
-  close(archive) # tidy up
-  Base.GC.gc()
-  return Arrow.Table(ARROW_FILENAME) |> DataFrame
+  open(ARROW_FILENAME, "w") do io
+    Arrow.write(io, DataFrame(itr); compress = :zstd, file = true)
+  end
+  close(archive)
+  return ARROW_FILENAME
 end
 
-# function clean_hmda_data()
-#   LOANTYPE_LEVELS = ["Conventional", "FHA", "VA-USDA"]
-#   ETHNICITY_LEVELS = ["not HL", "HL"]
-#   RACE_LEVELS = ["White", "Asian", "AA"]
-#   AGE_LEVELS = ["< 25", "25 to 34", "35 to 44", "45 to 54", "55 to 64", "> 64"]
+function load_conus_shapefile()
+  global SHAPEFILE
+  shapefile = Shapefile.Table(SHAPEFILE) |> DataFrame
+  transform!(shapefile, :geometry => ByRow(GO.centroid) => [:lat, :long])
+  filter!(:STATEFP => !in([02, 15, 60, 66, 69, 72, 78]), shapefile)
+  return shapefile
+end
 
-#   tbl = DataFrame(
-#     denial = Int[],
-#     loanterm = Int[],
-#     loantype = empty_factor(String, LOANTYPE_LEVELS),
-#     ethnicity = empty_factor(String, ETHNICITY_LEVELS),
-#     race = empty_factor(String, RACE_LEVELS),
-#     age = empty_factor(String, AGE_LEVELS; ordered = true),
-#     income = Float64[],
-#     dti = Float64[],
-#     tpop = Float64[],
-#     tminority = Float64[],
-#     tincome = Float64[],
-#     mincome = Float64[],
-#     lat = Float64[],
-#     long = Float64[],
-#   )
-# end
+function hmda2019_clean()
+  global ARROW_FILENAME, ARROW_CLEANED
 
+  # get coordinates
+  shapefile = load_conus_shapefile()
+  select!(shapefile, [:GEOID, :lat, :long])
 
+  # add coordinates to HMDA
+  arrow = Arrow.Table(ARROW_FILENAME)
+  tmp = TableOperations.filter(included_in_study, arrow) |> DataFrame
+  tmp = innerjoin(tmp, shapefile, on = [:census_tract => :GEOID])
+
+  hmda = DataFrame()
+
+  # RESPONSE
+  # convert response to 0/1 scale, 1 - loan denied
+  hmda.Denial = tmp.action_taken .== 3
+  # ---------------------------------------------------
+
+  # LOAN INFO
+  hmda.LoanTerm = parse.(Int16, tmp.loan_term)
+  hmda.LoanType = CategoricalArrays.recode(tmp.loan_type,
+    1   => "Conventional", # baseline
+    2   => "FHA",
+    3:4 => "VA-USDA"
+  ) |> CategoricalVector
+
+  # APPLICANT INFO
+  hmda.Ethnicity = CategoricalArrays.recode(tmp.derived_ethnicity,
+    "Hispanic or Latino"      => "HL",
+    "Not Hispanic or Latino"  => "NHL" # baseline
+  ) |> CategoricalVector
+  hmda.Race = CategoricalArrays.recode(tmp.derived_race,
+    "White"                     => "White", # baseline
+    "Asian"                     => "Asian",
+    "Black or African American" => "AA",
+  ) |> CategoricalVector
+  hmda.Sex = CategoricalArrays.recode(tmp.derived_sex,
+    "Male"    => "Male", # baseline
+    "Female"  => "Female",  
+  ) |> CategoricalVector
+  hmda.Age = CategoricalArrays.recode(tmp.applicant_age,
+    "<25"   => "<25", # baseline
+    "25-34" => "25-34",
+    "35-44" => "35-44",
+    "45-54" => "45-54",
+    "55-64" => "55-64",
+    "65-74" => ">64",
+    ">74"   => ">64",
+  ) |> CategoricalVector
+  hmda.Income = parse.(Int, tmp.income)
+  hmda.DTI = parse.(Int, tmp.debt_to_income_ratio)
+
+  # CENSUS TRACT DEMOGRAPHIC CHARACTERISTICS
+  hmda.Tpop = tmp.tract_population
+  hmda.Tminority = tmp.tract_minority_population_percent
+  hmda.Tincome = tmp.tract_to_msa_income_percentage
+  hmda.Mincome = tmp.ffiec_msa_md_median_family_income
+
+  # SPATIAL COORDINATES
+  hmda.Lat = tmp.lat
+  hmda.Long = tmp.long
+
+  # only non-negative income values; reporting for this and DTI is confusing
+  # see for example: https://mycomplianceresource.com/forums/topic/hmda-dti/
+  filter!(:Income => >=(0), hmda)
+
+  open(ARROW_CLEANED, "w") do io
+    Arrow.write(io, hmda; compress = :zstd, file = true)
+  end
+  return ARROW_CLEANED
+end
+
+function extract_shapefile_boundary(; tol = 0.1)
+  global VSIZIP_SHAPEFILE
+  
+  # STATEFP corresponding to Alaska, Hawaii, US territories
+  excluded = [02, 15, 60, 66, 69, 72, 78]
+
+  # Collect geometries for census tracts
+  geoms = MultiPolygon[]
+  AD.read(VSIZIP_SHAPEFILE) do dataset
+    layer = AD.getlayer(dataset, 0)
+    for feature in layer
+      key = parse(Int, AD.getfield(feature, "STATEFP"))
+      if key in excluded continue end
+      geom = GI.convert(LibGEOS,
+        AD.forceto(AD.getgeom(feature), AD.wkbMultiPolygon)
+      )
+      push!(geoms, geom)
+    end
+  end
+
+  # Combine into a single MultiPolygon with a cascaded union
+  multipoly = geoms |> GeometryCollection |> LibGEOS.unaryUnion
+  
+  # Keep only the largest geometry by area; this should be the US without tiny islands
+  areas = [LibGEOS.area(LibGEOS.getGeometry(multipoly, i)) for i in 1:LibGEOS.numGeometries(multipoly)]
+  out = LibGEOS.topologyPreserveSimplify(LibGEOS.getGeometry(multipoly, argmax(areas)), tol)
+  return out
+end
+
+function save_boundary_csv(out::Polygon, label)
+  # Save a plot to check!
+  fig = CairoMakie.plot(out)
+  CairoMakie.save("USBoundary_$(label).pdf", fig)
+  coords = LibGEOS.boundary(out) |> LibGEOS.getCoordSeq |> LibGEOS.getCoordinates
+  open("USBoundary_$(label).csv", "w") do io
+    CSV.write(io, [], writeheader = true, header = ["x", "y"])
+    for (x, y) in coords
+      CSV.write(io, (; x = [x], y = [y]), append = true)
+    end
+  end
+end
+
+function load_conus_triangulation(filepath)
+  boundary_points = [(row[1], row[2]) for row in eachrow(CSV.read(filepath, DataFrame))]
+  boundary_unique = unique(boundary_points)
+  lookup = Dict(key => i for (i, key) in enumerate(boundary_unique))
+  curve = map(key -> lookup[key], boundary_points)
+  tri = triangulate(boundary_unique, boundary_nodes = reverse(curve))
+  init_area = get_area(tri)
+  DelaunayTriangulation.refine!(tri, min_angle = 30.0, max_area = 0.001 * init_area)
+  DelaunayTriangulation.add_ghost_triangles!(tri)
+  return tri
+end
+
+function is_inside_mesh(Δ::Triangulation, s)
+  triangle = find_triangle(Δ, s, concavity_protection = true)
+ return all(>(0), triangle)
+end
+
+is_inside_mesh(Δ::Triangulation) = Base.Fix1(is_inside_mesh, Δ)
+
+function run_benchmark(; seed = 1903, sample_pct = 0.1)
+  Random.seed!(seed)
+
+  # Load triangulations
+  Δs = (
+    load_conus_triangulation(joinpath("USBoundary_tol=0.5.csv")),
+    load_conus_triangulation(joinpath("USBoundary_tol=0.1.csv")),
+    load_conus_triangulation(joinpath("USBoundary_tol=0.01.csv"))
+  );
+
+  # Load data and drop cases with dubious values
+  tbl_full = Arrow.Table(joinpath("data", "hmda2019_clean.arrow")) |> DataFrame
+  tbl_full = filter(row -> row.Income > 0 && row.DTI > 0 && row.Tpop > 0 && row.Tincome > 0 && row.Mincome > 0, tbl_full)
+  
+  # Sample the data to get down to a more manageable size
+  n_full = nrow(tbl_full)
+  n_sample = round(Int, n_full * sample_pct)
+  mask = zeros(Bool, n_full)
+  mask[1:n_sample] .= 1
+  shuffle!(mask)
+  tbl = tbl_full[mask, :]
+
+  # Build model from DataFrame. Categorical variables saved as CategoricalVector.
+  formula = @formula(0 ~
+    LoanTerm + LoanType +
+    Ethnicity + Race + Sex + Age +
+    log(Income) + log(DTI) + log(Tpop) + 
+    Tminority + log(Tincome) + log(Mincome)
+  )
+  mf = ModelFrame(formula, tbl)
+  mm = ModelMatrix(mf)
+
+  # Build response, model matrix, and spatial matrix
+  X_sample = mm.m
+  S_sample = [tbl.Lat tbl.Long] |> Transpose |> Matrix
+  y_sample = Float64.(tbl.Denial)
+
+  results = NamedTuple[]
+  for Δ in Δs
+    # Make sure locations lie in this version of the mesh!
+    idx = map(is_inside_mesh(Δ), eachcol(S_sample))
+    y, X, S = y_sample[idx], X_sample[idx, :], S_sample[:, idx]
+
+    # Run the benchmark
+    timed_result = @timed SpatialRegression.fitmodel(y, X, S, Δ;
+      family = Binomial(),
+      link = LogitLink(),
+      rho = 1.0,
+      tol = 1e-6,
+      backtrack = 100,
+      maxiter = 10^4
+    )
+
+    # Make predictions and check how we did.
+    n, p = sum(idx), size(X, 2) - 1
+    niter, tobs, vmod = timed_result.value
+    logl = SpatialRegression.eval_loglikelihood(tobs, vmod)
+    timing = timed_result.time
+    yhat = SpatialRegression.predict(X, S, vmod, Δ)
+    rmse_resp = sqrt(mean(abs2, y - yhat))
+    result = (;
+      cases = n,
+      features = p,
+      response = y,
+      location = S,
+      triangulation = Δ,
+      prediction = yhat,
+      niter, logl, timing, rmse_resp
+    )
+    push!(results, result)
+  end
+
+  return results
+end
+
+#
+# TRIANGULATIONS
+#
+function get_tri_title(Δ)
+  stats = statistics(Δ)
+  return "$(stats.num_solid_triangles) triangles, $(stats.num_solid_vertices) vertices"
+end
+
+function plot_compare_fitted(instances; markersize = 4.0, kwargs...)
+  local W = 400; H = 250
+  local NROW = 2; NSCENARIO = length(instances)
+
+  # Initialize figure with column labels
+  fig = Figure(size = (W * NSCENARIO, H * NROW))
+  Label(fig[2,1], "Observed", font = :bold, rotation = pi/2, tellheight = false)
+  Label(fig[3,1], "Predicted", font = :bold, rotation = pi/2, tellheight = false)
+  cr = Observable((0.0, 1.0))
+  for (j, prob) in enumerate(instances)
+    # Retrieve information about the instance
+    y, yhat, Δ, S = prob.response, prob.prediction, prob.triangulation, prob.location
+
+    # Retrive the boundary
+    nodes = DelaunayTriangulation.get_boundary_nodes(Δ)
+    pts = DelaunayTriangulation.get_points(Δ)
+    path = pts[nodes]
+
+    # Update minimum and maximum values for color scale
+    cr[] = (min(minimum(y), minimum(yhat)), max(maximum(y), maximum(yhat)))
+
+    # Add a row label with triangulation characteristics
+    Label(fig[1,1+j], get_tri_title(Δ), font = :bold, tellwidth = false)
+
+    # Add a panel for observed data
+    ax = Axis(fig[2,1+j])
+    lines!(ax, path, color = :black)
+    scatter!(ax, S[1,:], S[2,:]; colormap = Reverse(:redblue), color = y, colorrange = cr, markersize, kwargs...)
+
+    # Predicted
+    ax = Axis(fig[3,1+j])
+    lines!(ax, path, color = :black)
+    scatter!(ax, S[1,:], S[2,:]; colormap = Reverse(:redblue), color = yhat, colorrange = cr, markersize, kwargs...)
+  end
+  Colorbar(fig[2:3, 2+NSCENARIO], colormap = Reverse(:redblue), colorrange = cr, vertical = true)
+  return fig
+end
+
+results = run_benchmark(; sample_pct = 0.1, seed = 1903)
+plot_compare_fitted(results; markersize = 4)
+
+save("USLoans_n=10pct.pdf", current_figure())
+
+tbl = DataFrame()
+
+for r in results
+  stats = statistics(r.triangulation)
+  push!(
+    tbl,
+    (;
+      triangles = stats.num_solid_triangles,
+      vertices = stats.num_solid_vertices,
+      n = r.cases,
+      p = r.features,
+      time = r.timing / 60,   # record in minutes
+      niters = r.niter,
+      logl = r.logl,
+      rmse_resp = r.rmse_resp,
+    )
+  )
+end
+
+open("USLoans_n=10pct.txt", "w") do io
+  pretty_table(io, tbl; backend = :latex)
+end
