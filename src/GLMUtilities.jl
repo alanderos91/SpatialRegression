a14e0f1597fe5c9196c4cd187a529e11d503103e
Alfonso Landeros <alandero@ucr.edu>
2026-04-17 05:54:02 -0700

    Move `logpdf` and "stable eval" code into GLMUtilities; add mixtures
    - Need to evaluate full log-likelihoods, can't rely on deviance.
    - Use GLMUtilities.logpdf. Must always be qualified. Reason is that
      distributions like Normal() require that we handle the dispersion
      parameter carefully as it is coupled to coefficients in surrogate.
    - WARNING: We use inverse variance as "dispersion"! Normal pdf is
      written with that parameterization in mind for concavity.
    - Add deviance and dispersion utilities.
    - Add code for creating components in a mixture distribution. Found it
      useful in checking toy models/code.
    - The stable evaluation code uses the log-sum-exp trick correctly now,
      and the code is much more simplified. Note that underflow of convex
      weights involving log-likelihoods MUST be handled by the caller!

--- src/GLMUtilities.jl
+++ src/GLMUtilities.jl
@@ -5,51 +5,80 @@ module GLMUtilities
 using LogExpFunctions
 using SpecialFunctions
 using Distributions: 
-  Distribution, UnivariateDistribution,
-  Normal, Bernoulli, Binomial, Poisson
+  Distribution, UnivariateDistribution, MixtureModel,
+  Normal, Bernoulli, Binomial, Poisson,
+  components, component_type
 using GLM:
   Link,
   IdentityLink, LogitLink, LogLink,
   canonicallink, glm, coef
 
-export Distribution, UnivariateDistribution,
+export Distribution, UnivariateDistribution, MixtureModel,
   Normal, Bernoulli, Binomial, Poisson,
+  components, component_type,
   Link,
   IdentityLink, LogitLink, LogLink,
   canonicallink
 #
 # LOG-LIKELIHOOD: Use mean parameter only
 #
-log_likelihood(y, mu, ::Normal, n = one(y)) = -1//2*abs2(y - mu)
-log_likelihood(y, mu, ::Bernoulli, n = one(y)) = isone(y) ? log(mu) : log(1-mu)
-log_likelihood(y, mu, ::Binomial, n = one(y)) = begin
+logpdf(y, mu, phi, ::Normal, n = one(y)) = -1//2 * (abs2(y - mu) * phi + log(2*π/phi))
+logpdf(y, mu, phi, ::Bernoulli, n = one(y)) = isone(y) ? log(mu) : log(1-mu)
+logpdf(y, mu, phi, ::Binomial, n = one(y)) = begin
   if isone(n)
-    log_likelihood(y, mu, Bernoulli(), n)
+    logpdf(y, mu, phi, Bernoulli(), n)
   else
     y * log(mu) + (n-y) * log(1-mu) + loggamma(n+1) - loggamma(y+1) - loggamma(n-y+1)
   end
 end
-log_likelihood(y, mu, ::Poisson, n = one(y)) = y * log(mu) - mu - loggamma(y+1)
+logpdf(y, mu, phi, ::Poisson, n = one(y)) = y * log(mu) - mu - loggamma(y+1)
 #
 # LOG-LIKELIHOOD: Use linear predictor to avoid -Inf/NaN
 #
-log_likelihood(y, mu, eta, family::Distribution, link::Link, n = one(y)) = log_likelihood(y, mu, family, n)
-log_likelihood(y, mu, eta, ::Bernoulli, ::LogitLink, n = one(y)) = begin
+logpdf(y, mu, phi, eta, family::Distribution, link::Link, n = one(y)) = logpdf(y, mu, phi, family, n)
+logpdf(y, mu, phi, eta, ::Bernoulli, ::LogitLink, n = one(y)) = begin
   if abs(eta) ≥ 32
     isone(y) ? loglogistic(eta) : log1mlogistic(eta)
   else
-    log_likelihood(y, mu, Bernoulli(), n)
+    logpdf(y, mu, phi, Bernoulli(), n)
   end
 end
-log_likelihood(y, mu, eta, ::Binomial, ::LogitLink, n = one(y)) = begin
+logpdf(y, mu, phi, eta, ::Binomial, ::LogitLink, n = one(y)) = begin
   if isone(n)
-    log_likelihood(y, mu, eta, Bernoulli(), LogitLink(), n)
+    logpdf(y, mu, phi, eta, Bernoulli(), LogitLink(), n)
   else
     y * loglogistic(eta) + (n-y) * log1mlogistic(eta) + loggamma(n+1) - loggamma(y+1) - loggamma(n-y+1)
   end
 end
 
-export log_likelihood
+export logpdf
+#
+# DEVIANCE
+#
+deviance(::Normal, y, mu) = abs2(y - mu)
+deviance(::Bernoulli, y, mu) = -2 * (isone(y) ? log(mu) : log1p(-mu))
+deviance(::Binomial, y, mu) = begin
+  if y <= 1 # 0 <= y <= 1
+    deviance(Bernoulli(), y, mu)
+  else y > 1
+    2 * (y * (log(y) - log(mu)) + (1-y) * (log1p(-y) - log1p(-mu)))
+  end
+end
+deviance(::Poisson, y, mu) = 2 * (xlogy(y, y / mu) - (y - mu))
+
+export deviance
+#
+# DISPERSION
+#
+needs_dispersion(::Distribution) = false
+needs_dispersion(::Normal) = true
+
+dispfun(d::Distribution, phi) = begin
+  needs_dispersion(d) ? _dispfun(d, phi) : one(phi)
+end
+_dispfun(::Normal, phi) = phi
+
+export needs_dispersion, dispfun
 #
 # (INVERSE) LINK FUNCTIONS
 #
@@ -105,5 +134,50 @@ mustart(::Poisson, y) = begin
 end
 
 export mustart
+#
+# MIXTURES
+#
+function create_component(::Normal, link, η, φ)
+  σ = sqrt(φ)
+  μ = meanfun(link, η)
+  return Normal(μ, σ)
+end
+
+function create_component(::Binomial, link, η, φ)
+  return Bernoulli(meanfun(link, η))
+end
+
+function create_component(::Poisson, link, η, φ)
+  return Poisson(meanfun(link, η))
+end
+
+export create_component
+#
+# NUMERICAL STABILITY
+#
+function stable_irls_weight(family, link, η, μ, dμdη)
+  return cancancel(family, link) ? dμdη : abs2(dμdη) / varfun(family, μ)
+end
+
+function weighted_sumexp(alpha, logf, m = argmax(logf))
+  logf_m = logf[m]
+  s = zero(eltype(logf))
+  for (a_k, logf_k) in zip(alpha, logf)
+    s += a_k * exp(logf_k - logf_m)
+  end
+  return s, logf_m
+end
+
+function weighted_logsumexp(alpha, logf, m = argmax(logf))
+  s, logf_m = weighted_sumexp(alpha, logf, m)
+  return log(s) + logf_m
+end
+
+function stable_convex_weight(k::Integer, alpha, logf)
+  s, logf_m = weighted_sumexp(alpha, logf)
+  return alpha[k]*exp(logf[k] - logf_m) / s
+end
+
+export stable_irls_weight, weighted_sumexp, weighted_logsumexp, stable_convex_weight
 
 end # module
